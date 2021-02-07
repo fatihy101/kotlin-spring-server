3ba688d206209a914f7a9e7bdd056fc7971f9037
Fatih <fatihy@yahoo.com>
2021-02-07 04:01:35 +0300

    Fotoğraf yükleme servisi Webflux'a uygun hale getirildi.

--- src/main/kotlin/com/enstrurental/server/controllers/PhotosController.kt
+++ src/main/kotlin/com/enstrurental/server/controllers/PhotosController.kt
@@ -2,20 +2,27 @@ package com.enstrurental.server.controllers
 
 import org.springframework.web.bind.annotation.*
 import com.enstrurental.server.services.PhotoService
-import org.bson.types.Binary
 import org.springframework.http.ResponseEntity
-import org.springframework.web.multipart.MultipartFile
+import org.springframework.http.codec.multipart.FilePart
+import org.springframework.http.codec.multipart.Part
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import java.util.*
+import org.springframework.web.bind.annotation.RequestMapping
+
 
 @RestController
 @RequestMapping("photos")
 class PhotosController (val photoService: PhotoService) {
-
+    // BUG: Something wrong with the image BSON data.
     @CrossOrigin
     @PostMapping("/save")
     fun savePhoto(@RequestPart("id") id: String,
-                  @RequestPart("image") image: MultipartFile): Mono<String> = photoService.addPhoto(id, image)
+                  @RequestPart("image") image: Flux<Part>) : Flux<String>  = image
+        .filter { part -> part is FilePart }
+        .ofType(FilePart::class.java)
+        .flatMap { data -> photoService.addPhoto(id, data) }
+
 
     @CrossOrigin
     @GetMapping("get/{id}")
--- src/main/kotlin/com/enstrurental/server/services/PhotoService.kt
+++ src/main/kotlin/com/enstrurental/server/services/PhotoService.kt
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 import org.bson.BsonBinarySubType
 import org.bson.types.Binary
+import org.springframework.http.codec.multipart.FilePart
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 import java.io.IOException
@@ -16,10 +18,11 @@ class PhotoService {
     @Autowired
     lateinit var photosRepository: PhotosRepository
 
-    fun addPhoto(id: String, file: MultipartFile): Mono<String> {
-        val photo = Photos(id, Binary(BsonBinarySubType.BINARY, file.bytes))
-        return photosRepository.save(photo).map { saved_photo -> saved_photo.id }
-    }
+    fun addPhoto(id: String, filePart: FilePart): Mono<String>  = filePart.content().flatMap { dataBuffer ->
+            val bytes = ByteArray(dataBuffer.readableByteCount())
+            val photo = Photos(id, Binary(BsonBinarySubType.BINARY, bytes))
+            photosRepository.save(photo).map { saved_photo -> saved_photo.id }
+        }.last()
 
     fun getPhoto(id:String): Mono<Photos> = photosRepository.findById(id)
 }
